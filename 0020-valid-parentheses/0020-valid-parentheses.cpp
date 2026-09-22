59d4d4c865f5cae0b1563e44c50b19b0404ef548
Vivek Kumar Chaurasiya <vivekchaurasiyatutorials@gmail.com>
2026-09-22 08:18:00 +0530

    Time: 3 ms (9.32%), Space: 9 MB (36.93%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for(char ch:s){
+            if(ch=='{' || ch=='[' || ch=='('){
+                st.push(ch);
+            }else{
+                  if (st.empty())
+                    return false;
+
+                if((st.top() == '[' && ch == ']') || (st.top() =='{' && ch =='}')  || (st.top() =='(' && ch==')')){
+                    st.pop();
+                    continue;
+                }else{
+                    return false;
+                }
+            }
+        }
+        return st.empty();
+    }
+};
+
